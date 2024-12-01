69e9b2384d6d00e01afdb30774a1434a9fb7b199
Tim Millard <timwmillard@gmail.com>
2024-12-02 00:13:06 +1100

    rename folder to 01

---
+++ 01/day1_part2.sql
@@ -0,0 +1,14 @@
+with left_col as (
+    select 
+        row_number() over (order by left_value) as row_id,
+        left_value as left_value
+    from day1.input
+), right_col as (
+    select right_value, count(right_value)
+    from day1.input
+    group by right_value
+)
+select sum(l.left_value * coalesce(r.count, 0))
+from left_col l
+left join right_col r on l.left_value = r.right_value
+
