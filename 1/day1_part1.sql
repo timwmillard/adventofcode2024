cec9df4adb4a220a48fe47eb70b15eb7099d3c32
Tim Millard <timwmillard@gmail.com>
2024-12-01 23:40:02 +1100

    Day1 solved in SQL

---
+++ 1/day1_part1.sql
@@ -0,0 +1,15 @@
+with left_col as (
+    select 
+        row_number() over (order by left_value) as row_id,
+        left_value as left_value
+    from day1.input
+), right_col as (
+    select 
+        row_number() over (order by right_value) as row_id,
+        right_value as right_value
+    from day1.input
+)
+select sum(abs(left_value - right_value))
+from left_col
+join right_col using (row_id);
+
